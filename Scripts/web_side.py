a745613056d3158240067ce8863386d17a3fe1dd
Andres Gonzalez <andrewgonzlez@gmail.com>
2024-07-03 11:12:24 -0500

    correcciones en la ejecucion, navegador incognito y nuevo contexto por cada uno de los registros

--- Scripts/web_side.py
+++ Scripts/web_side.py
@@ -1,14 +1,17 @@
 def run_browser(playwright, link, df, form_settings_df, social_dict):
 
-    browser = playwright.chromium.launch(headless=True)
-
-    page = browser.new_page()
-
-    page.goto(link, wait_until='networkidle')
-
     df_columns = df.columns
 
     for index, row in df.iterrows():
+
+        browser = playwright.chromium.launch(headless=True)
+
+        context = browser.new_context()
+
+        page = context.new_page()
+
+        page.goto(link, wait_until='networkidle')
+
         for col in df_columns:
 
             field = col
@@ -25,13 +28,33 @@ def run_browser(playwright, link, df, form_settings_df, social_dict):
                     social_dict['campo_excel'] == value
                 ]['campo_form'].to_list()[0]
                 fill_selector(col, page, selection)
-        page.click('input[type="submit"][value="Enviar Respuesta"]')
-        page.wait_for_load_state('load')
+
+        page.evaluate(
+            """
+            () => {
+                document.getElementsByClassName('btn')[0].click();
+                console.log('clicked!');
+            }
+            """
+        )
+
+        page.wait_for_selector(
+            '//p[text()="GRACIAS POR SU REGISTRO"]', timeout=10000)
+
+        page.evaluate(
+            """
+            () => {
+                document.cookie.split(';').forEach(function(c) { 
+                    document.cookie = c.trim().split("=")[0] + '=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/'; 
+                });
+            }
+            """
+        )
+        browser.close()
+
         print(f'{index+1} cargado')
 
     print('\nHe cargado todos los registros')
-
-    browser.close()
 
 
 def fill_input(label_name, page, value):
